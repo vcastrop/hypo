26f161f27c9981638cd2c3f229b2039b14bc9b8a
EmanuelGoQ <egonzalezq@eafit.edu.co>
2026-09-06 21:45:42 -0500

    fix(docker): fix services startup in development environment

--- workload/demo-app/microservices/order-service/db.js
+++ workload/demo-app/microservices/order-service/db.js
@@ -7,7 +7,6 @@ const pool = new Pool({
   database: process.env.DB_NAME || 'postgres',
   password: process.env.DB_PASS || 'postgres',
   port: process.env.DB_PORT || 5432,
-  ssl: { rejectUnauthorized: false },
 });
 
 export const initDB = async () => {
